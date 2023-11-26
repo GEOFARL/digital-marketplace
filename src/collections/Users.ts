bc91d866ee9b188a47d5854b650ce1edf9215fd0
Maxim <toper.one111@gmail.com>
2023-11-26 23:53:52 +0200

    Send verification email letter

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -5,7 +5,7 @@ export const Users: CollectionConfig = {
   auth: {
     verify: {
       generateEmailHTML: ({ token }) => {
-        return `<a href="${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}"></a>`;
+        return `<a href="${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}">Verify Your Email</a>`;
       },
     },
   },
--- src/trpc/authRouter.ts
+++ src/trpc/authRouter.ts
@@ -2,6 +2,7 @@ import { publicProcedure, router } from './trpc';
 import { getPayloadClient } from '../getPayload';
 import { AuthCredentialsValidator } from '../lib/validators/accountCredentialsValidator';
 import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
 
 export const authRouter = router({
   createPayloadUser: publicProcedure
@@ -34,5 +35,24 @@ export const authRouter = router({
       });
 
       return { success: true, sentToEmail: email };
+    }),
+
+  verifyEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .query(async ({ input }) => {
+      const { token } = input;
+
+      const payload = await getPayloadClient();
+
+      const isVerified = await payload.verifyEmail({
+        collection: 'users',
+        token,
+      });
+
+      if (!isVerified) {
+        throw new TRPCError({ code: 'UNAUTHORIZED' });
+      }
+
+      return { success: true };
     }),
 });
