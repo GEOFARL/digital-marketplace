186e4a0a6a6a560aa71ca495224e98c8f3a12d1e
Maxim <toper.one111@gmail.com>
2023-11-26 16:07:38 +0200

    Create a ui for the sign up form

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { Icons } from '@/components/Icons';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { cn } from '@/lib/utils';
+import { Label } from '@radix-ui/react-label';
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
+
+const Page = () => {
+  return (
+    <>
+      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col items-center space-y-2 text-center">
+            <Icons.logo className="w-20 h-20" />
+            <h1 className="text-2xl font-bold">Create an account</h1>
+
+            <Link
+              href="/sign-in"
+              className={buttonVariants({
+                variant: 'link',
+                className: 'gap-1.5',
+              })}
+            >
+              Already have an account? Sign-in
+              <ArrowRight className="h-4 w-4" />
+            </Link>
+          </div>
+
+          <div className="grid gap-6">
+            <form onSubmit={() => {}}>
+              <div className="gird gap-2">
+                <div className="grip gap-1 py-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    className={cn({
+                      'focus-visible:ring-red-500': true,
+                    })}
+                    placeholder="you@example.com"
+                    id="email"
+                  />
+                </div>
+                <div className="grip gap-1 py-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    className={cn({
+                      'focus-visible:ring-red-500': true,
+                    })}
+                    placeholder="*********"
+                    id="password"
+                  />
+                </div>
+
+                <Button className="w-full mt-2">Sign Up</Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
