446e38146a576dc538bb364dcb2dcfb917ac0759
Maxim <toper.one111@gmail.com>
2023-11-26 10:18:39 +0200

    Add sign in and sign up to the nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,8 +2,11 @@ import Link from 'next/link';
 import MaxWidthWrapper from './MaxWidthWrapper';
 import { Icons } from './Icons';
 import NavItems from './NavItems';
+import { buttonVariants } from './ui/button';
 
 const NavBar = () => {
+  const user = null;
+
   return (
     <div className="bg-white sticky z-50 top-0 inset-0 h-16">
       <header className="relative bg-white">
@@ -20,6 +23,44 @@ const NavBar = () => {
 
               <div className="hidden z-50 lg: ml-8 lg:block lg:self-stretch">
                 <NavItems />
+              </div>
+
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {!user && (
+                    <Link
+                      href={'/sign-in'}
+                      className={buttonVariants({ variant: 'ghost' })}
+                    >
+                      Sign in
+                    </Link>
+                  )}
+
+                  {!user && (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden />
+                  )}
+
+                  {user ? (
+                    <h1>hi</h1>
+                  ) : (
+                    <Link
+                      href={'/sign-up'}
+                      className={buttonVariants({ variant: 'ghost' })}
+                    >
+                      Create account
+                    </Link>
+                  )}
+
+                  {user && (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden />
+                  )}
+
+                  {!user && (
+                    <div className="flex lg:ml-6">
+                      <span className="h-6 w-px bg-gray-200" aria-hidden />
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
