f05d7bd509b284fbefac376e788d01c12512a544
Maxim <toper.one111@gmail.com>
2023-11-26 08:58:02 +0200

    Create NavItem component and make it open and close

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { PRODUCT_CATEGORIES } from '@/config';
+import { Button } from './ui/button';
+import { ChevronDown } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+type Category = (typeof PRODUCT_CATEGORIES)[number];
+
+interface NavItemProps {
+  category: Category;
+  handleOpen: () => void;
+  isOpen: boolean;
+  isAnyOpen: boolean;
+}
+
+const NavItem: React.FC<NavItemProps> = ({
+  category,
+  handleOpen,
+  isAnyOpen,
+  isOpen,
+}) => {
+  return (
+    <div className="flex">
+      <div className="relative flex items-center">
+        <Button
+          className="gap-1.5"
+          onClick={handleOpen}
+          variant={isOpen ? 'secondary' : 'ghost'}
+        >
+          {category.label}{' '}
+          <ChevronDown
+            className={cn('h-4 w-4 transition-all text-muted-foreground', {
+              '-rotate-180': isOpen,
+            })}
+          />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NavItem;
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,11 +1,37 @@
 'use client';
 
+import { PRODUCT_CATEGORIES } from '@/config';
 import { useState } from 'react';
+import NavItem from './NavItem';
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
-  return <div className="flex gap-4 h-full">NavItems</div>;
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+
+        const isOpen = activeIndex === i;
+
+        return (
+          <NavItem
+            key={category.value}
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={activeIndex !== null}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
