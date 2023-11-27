0151a5cebc1f92126e7b1713da68d2114c068c91
Maxim <toper.one111@gmail.com>
2023-11-27 19:27:13 +0200

    Create product details page

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,5 +1,11 @@
+import ImageSlider from '@/components/ImageSlider';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import { PRODUCT_CATEGORIES } from '@/config';
+import { getPayloadClient } from '@/getPayload';
+import { formatPrice } from '@/lib/utils';
+import { Check } from 'lucide-react';
 import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
 interface PageProps {
   params: {
@@ -12,12 +18,35 @@ const BREADCRUMBS = [
   { id: 2, name: 'Products', href: '/products' },
 ];
 
-const Page = ({ params }: PageProps) => {
+const Page = async ({ params }: PageProps) => {
+  const payload = await getPayloadClient();
+
+  const { docs: products } = await payload.find({
+    collection: 'products',
+    limit: 1,
+    where: {
+      id: { equals: params.productId },
+      approvedForSale: {
+        equals: 'approved',
+      },
+    },
+  });
+
+  const [product] = products;
+  if (!product) return notFound();
+
+  const label = PRODUCT_CATEGORIES.find(
+    ({ value }) => value === product.category
+  )?.label;
+
+  const validUrls = product.images
+    .map(({ image }) => (typeof image === 'string' ? image : image.url))
+    .filter(Boolean) as string[];
+
   return (
     <MaxWidthWrapper className="bg-white">
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-          {/* Product Details */}
           <div className="lg:max-w-lg lg:self-end">
             <ol className="flex items-center space-x-2">
               {BREADCRUMBS.map((breadCrumb, i) => (
@@ -43,6 +72,46 @@ const Page = ({ params }: PageProps) => {
                 </li>
               ))}
             </ol>
+
+            <div className="mt-4">
+              <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                {product.name}
+              </h1>
+            </div>
+
+            <section className="mt-4">
+              <div className="flex items-center">
+                <p className="font-medium text-gray-900">
+                  {formatPrice(product.price)}
+                </p>
+
+                <div className="ml-4 border-l text-muted-foreground border-gray-300 pl-4">
+                  {label}
+                </div>
+              </div>
+
+              <div className="mt-4 space-y-6">
+                <p className="text-base text-muted-foreground">
+                  {product.description}
+                </p>
+              </div>
+
+              <div className="mt-6 flex items-center">
+                <Check
+                  aria-hidden
+                  className="h-5 w-5 flex-shrink-0 text-green-500"
+                />
+                <p className="ml-2 text-sm text-muted-foreground">
+                  Eligible for instant delivery
+                </p>
+              </div>
+            </section>
+          </div>
+
+          <div className="mt-10 lg:col-start-2 lg:row-start-2 lg:mt-0 lg:self-center">
+            <div className="aspect-square rounded-lg">
+              <ImageSlider urls={validUrls} />
+            </div>
           </div>
         </div>
       </div>
