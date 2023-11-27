377670e40690bc902674bf7289f0d76d23e882fb
Maxim <toper.one111@gmail.com>
2023-11-27 11:44:23 +0200

    Model data for products and images

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,13 +6,15 @@ import path from 'path';
 import { Users } from './collections/Users';
 
 import dotenv from 'dotenv';
+import { Products } from './collections/Products/Products';
+import { Media } from './collections/Products/Media';
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
 });
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  collections: [Users],
+  collections: [Users, Products, Media],
   routes: {
     admin: '/sell',
   },
