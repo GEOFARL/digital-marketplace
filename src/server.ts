12af86cde82dc58e6920739f2a5dc6ea8ada8779
Maxim <toper.one111@gmail.com>
2023-11-28 14:38:15 +0200

    Create web hook for handling stripe response and a response email

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import { nextApp, nextHandler } from './nextUtils';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { appRouter } from './trpc';
 import { inferAsyncReturnType } from '@trpc/server';
+import bodyParser from 'body-parser';
+import { IncomingMessage } from 'http';
+import { stripeWebhookHandler } from './webHooks';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -18,7 +21,17 @@ const createContext = ({
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext>;
 
+export type WebhookRequest = IncomingMessage & { rawBody: Buffer };
+
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer;
+    },
+  });
+
+  app.post('/api/webhooks/stripe', webhookMiddleware, stripeWebhookHandler);
+
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
