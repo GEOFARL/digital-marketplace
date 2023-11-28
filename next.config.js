ec6a4f3b78e9aa8d9feacf984cfcc3ba09d73dfc
Maxim <toper.one111@gmail.com>
2023-11-29 00:10:36 +0200

    Update metadata

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: 'localhost',
-        pathname: '**',
-        port: '3000',
-        protocol: 'http',
-      },
+    domains: [
+      'localhost',
+      'https://digital-marketplace-production-2f92.up.railway.app/',
     ],
   },
 };
