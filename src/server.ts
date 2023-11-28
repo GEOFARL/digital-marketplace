e6920fafed9b6f1fa1815f64716dfddef9f5bae9
Maxim <toper.one111@gmail.com>
2023-11-28 19:58:45 +0200

    Protect cart route

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import { IncomingMessage } from 'http';
 import { stripeWebhookHandler } from './webHooks';
 import nextBuild from 'next/dist/build';
 import path from 'path';
+import { PayloadRequest } from 'payload/types';
+import { parse } from 'url';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -42,6 +44,22 @@ const start = async () => {
       },
     },
   });
+
+  const cartRouter = express.Router();
+
+  cartRouter.use(payload.authenticate);
+
+  cartRouter.get('/', (req, res) => {
+    const request = req as PayloadRequest;
+
+    if (!request.user) return res.redirect('/sign-in?origin=cart');
+
+    const parsedUrl = parse(req.url, true);
+
+    return nextApp.render(req, res, '/cart', parsedUrl.query);
+  });
+
+  app.use('/cart', cartRouter);
 
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
