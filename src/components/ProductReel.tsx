c591f600809c6f76861880014cf954c7f79d5507
Maxim <toper.one111@gmail.com>
2023-11-28 13:16:47 +0200

    Create thank for ordering page

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -43,7 +43,7 @@ const ProductReel = (props: ProductReelProps) => {
       <div className="md:flex md:items-center md:justify-between mb-4">
         <div className="max-w-2xl px-4 lg:max-w-4xl lg:px-0">
           {title && (
-            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+            <h1 className="text-2xl fon6t-bold text-gray-900 sm:text-3xl">
               {title}
             </h1>
           )}
