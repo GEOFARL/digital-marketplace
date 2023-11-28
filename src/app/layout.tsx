63de399464d953d5a0a45ad40dbb6c6f654f6fb6
Maxim <toper.one111@gmail.com>
2023-11-28 21:39:40 +0200

    Desing footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils';
 import NavBar from '@/components/NavBar';
 import Providers from '@/components/Providers';
 import { Toaster } from 'sonner';
+import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           <Providers>
             <NavBar />
             <div className="flex-grow flex-1">{children}</div>
+            <Footer />
           </Providers>
         </main>
 
